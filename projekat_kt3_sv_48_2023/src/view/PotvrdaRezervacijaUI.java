72d528d42cfa0d5b32473e985b065441daff41fc
BoskoVasilic <71883918+BoskoVasilic@users.noreply.github.com>
2024-06-06 01:25:10 +0200

    dodati filteri

--- projekat_kt3_sv_48_2023/src/view/PotvrdaRezervacijaUI.java
+++ projekat_kt3_sv_48_2023/src/view/PotvrdaRezervacijaUI.java
@@ -13,16 +13,27 @@ import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.TableRowSorter;
 
 import manage.RezervacijaManager;
+import manage.TipSobeManager;
+import manage.DodatnaUslugaManager;
 import model.RezervacijeModel;
-import java.awt.BorderLayout;
 import java.awt.Font;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JCheckBox;
+import javax.swing.ScrollPaneConstants;
 
 public class PotvrdaRezervacijaUI extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private RezervacijaManager rm;
+	private TipSobeManager tsm = new TipSobeManager("data/tipoviSoba.csv");
 	protected TableRowSorter<AbstractTableModel> tableSorter = new TableRowSorter<AbstractTableModel>();
+	private JPanel panelTipSobe;
+	private DodatnaUslugaManager dum = new DodatnaUslugaManager("data/dodatneUsluge.csv");
 
 	public PotvrdaRezervacijaUI() {
 		setTitle("Hotel - Pristigle rezervacije");
@@ -32,6 +43,7 @@ public class PotvrdaRezervacijaUI extends JFrame {
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 1000, 500);
 		setLocationRelativeTo(null);
+		setResizable(false);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
@@ -45,11 +57,83 @@ public class PotvrdaRezervacijaUI extends JFrame {
 		centerRenderer.setHorizontalAlignment( JLabel.CENTER );
 		table.setDefaultRenderer(String.class, centerRenderer);
 		tableSorter.setModel((AbstractTableModel) table.getModel());
-		contentPane.setLayout(new BorderLayout(0, 0));
+		contentPane.setLayout(null);
 		table.setRowSorter(tableSorter);
 		JScrollPane scrollPane = new JScrollPane(table);
+		scrollPane.setBounds(5, 5, 974, 215);
 		contentPane.add(scrollPane);
 		
+		JLabel lblNewLabel = new JLabel("Postavite status izabrane rezervacije:");
+		lblNewLabel.setBounds(10, 242, 242, 14);
+		contentPane.add(lblNewLabel);
+		
+		JButton btnNewButton = new JButton("POTVRĐENA");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton.setBounds(48, 270, 106, 23);
+		contentPane.add(btnNewButton);
+		
+		JButton btnOdbijena = new JButton("ODBIJENA");
+		btnOdbijena.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnOdbijena.setBounds(48, 304, 106, 23);
+		contentPane.add(btnOdbijena);
+		
+		JLabel lblNewLabel_1 = new JLabel("Filteri:");
+		lblNewLabel_1.setBounds(349, 242, 46, 14);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Tipovi sobe:");
+		lblNewLabel_2.setBounds(359, 265, 69, 14);
+		contentPane.add(lblNewLabel_2);
+		
+		panelTipSobe = new JPanel();
+		panelTipSobe.setBounds(360, 292, 145, 130);
+		panelTipSobe.setLayout(new BoxLayout(panelTipSobe, BoxLayout.Y_AXIS));
+		
+		tsm.ucitajTipoveSoba();
+		for (int i = 0; i < tsm.getTipoviSoba().size(); i++) {
+			JCheckBox chckbx = new JCheckBox(tsm.getTipoviSoba().get(i).getNaziv());
+			//chckbx.setBounds(360, 286 + (i * 23), 138, 23);
+			panelTipSobe.add(chckbx);
+		}
+		
+		JScrollPane scrollPaneCheckBox = new JScrollPane(panelTipSobe);
+		scrollPaneCheckBox.setBounds(360, 292, 145, 130);
+		scrollPaneCheckBox.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+		scrollPaneCheckBox.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		contentPane.add(scrollPaneCheckBox);
+		
+		JLabel lblNewLabel_2_1 = new JLabel("Dodatne usluge:");
+		lblNewLabel_2_1.setBounds(557, 265, 100, 14);
+		contentPane.add(lblNewLabel_2_1);
+		
+		JPanel panelDodatneusluge = new JPanel();
+		panelDodatneusluge.setBounds(360, 292, 145, 130);
+		panelDodatneusluge.setLayout(new BoxLayout(panelDodatneusluge, BoxLayout.Y_AXIS));
+		
+		dum.ucitajDodatneUsluge();
+		for (int i = 0; i < dum.getDodatneUsluge().size(); i++) {
+			JCheckBox chckbx = new JCheckBox(dum.getDodatneUsluge().get(i).getNaziv());
+			// chckbx.setBounds(360, 286 + (i * 23), 138, 23);
+			panelDodatneusluge.add(chckbx);
+		}
+		
+		
+		JScrollPane scrollPaneCheckBox_1 = new JScrollPane(panelDodatneusluge);
+		scrollPaneCheckBox_1.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+		scrollPaneCheckBox_1.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		scrollPaneCheckBox_1.setBounds(558, 292, 145, 130);
+		contentPane.add(scrollPaneCheckBox_1);
+		
+		JButton btnNewButton_1 = new JButton("Primeni");
+		btnNewButton_1.setBounds(614, 433, 89, 23);
+		contentPane.add(btnNewButton_1);
+		
+	    
 	}
-
 }
---
+++ projekat_kt3_sv_48_2023/src/view/loginUI.java
@@ -0,0 +1,57 @@
+package view;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+
+public class loginUI extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+
+	public loginUI() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("DOBRODOŠLI");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 20));
+		lblNewLabel.setBounds(114, 11, 192, 53);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("PRIJAVA");
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblNewLabel_1.setBounds(165, 54, 87, 36);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Korisničko ime:");
+		lblNewLabel_2.setBounds(114, 115, 110, 14);
+		contentPane.add(lblNewLabel_2);
+		
+		textField = new JTextField();
+		textField.setBounds(114, 132, 192, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_2_1 = new JLabel("Lozinka:");
+		lblNewLabel_2_1.setBounds(114, 174, 110, 14);
+		contentPane.add(lblNewLabel_2_1);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(114, 191, 192, 20);
+		contentPane.add(textField_1);
+	}
+}
